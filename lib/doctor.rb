2409d6e4cae6ee2a8c58ed29855e4d722ec155e0
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-24 11:01:44 -0600

    commit tree 4f1d36ca448e545e39567e355cd40c06c08bd626

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,28 @@
+class Doctor
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
 
+    def appointments
+        Appointment.all.select do |a|
+            a.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def patients
+        appointments.map(&:patient)
+      end
+end
